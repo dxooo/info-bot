acec1d1ebe7acf79c1e62d94761cb34e5b1c4d62
Raymond2112 <41760656+Raymond2112@users.noreply.github.com>
2018-07-27 02:34:10 +0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -195,7 +195,7 @@ async def searchpc(ctx, *, arg):
     await ctx.send(found_list)	  
     embed = discord.Embed(title="__State Police Quick Links__", description="_Used to convey quick links to important information about the State Police_", color=0x3D59AB)
     embed.set_author(name="State Police Info Bot", icon_url="https://cdn.discordapp.com/attachments/393324031505465344/471855906699739136/sasp_logo_updated_2018.png")
-    embed.add_field(name=found_list, value="[Click Here]", inline=False)
+    embed.add_field(name=', '.join(found_list), value="[Click Here]", inline=False)
     await ctx.send(embed=embed)
    
 	
