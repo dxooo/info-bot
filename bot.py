dc204d005e674c1920c45957b00d06f1399cebe6
Raymond2112 <41760656+Raymond2112@users.noreply.github.com>
2018-07-27 13:20:21 +0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,8 +1,4 @@
 # This bot requires no permissions in order to work besides being able to read and write in whatever channel it's left in
-pip install beautifulsoup4
-pip install requests
-
-
 import discord
 from discord.ext import commands
 import os
