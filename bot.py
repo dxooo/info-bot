b8b9f7e8e7b735502aba85dc2bd4a67216209181
Raymond2112 <41760656+Raymond2112@users.noreply.github.com>
2018-07-27 12:33:08 +0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -188,7 +188,8 @@ async def searchpc(ctx, *, arg):
     embed.set_author(name="State Police Info Bot", icon_url="https://cdn.discordapp.com/attachments/393324031505465344/471855906699739136/sasp_logo_updated_2018.png")
     embed.add_field(name="Here's what we found based on your search:", value=', '.join(found_list), inline=False)
     await ctx.send(embed=embed)
-    await asyncio.sleep(10) 
+    await asyncio.sleep(2) 
+    await message.delete()
     await ctx.channel.delete_messages(embed)
 	
 @bot.command()
