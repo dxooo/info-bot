5b856bcec16fe77c5da2676c38d256c1343c30b7
Raymond2112 <41760656+Raymond2112@users.noreply.github.com>
2018-07-28 19:00:35 +0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -14,6 +14,9 @@ async def on_ready():
     print(bot.user.name)
     print(bot.user.id)
     print('------')
+    game = discord.Game("Assisting the State Police")
+    await bot.change_presence(status=discord.Status.online, activity=game)
+
 	
 # Greet new users and provide helpful information	
 @bot.event
@@ -49,6 +52,23 @@ async def links(ctx):
 	
     await ctx.send(embed=embed)
 
+
+@bot.command()
+async def embe(ctx,ar,arg,arg2,arg3,arg4):
+    user_inpt = arg  
+    user_inpt2 = arg2
+    user_inpt3 = arg3
+    user_inpt4 = arg4
+    embed = discord.Embed(title="".join(user_inpt), description="".join(user_inpt2), color=0x3D59AB)
+	
+    embed.set_author(name="{}".format(ar), icon_url="https://cdn.discordapp.com/attachments/393324031505465344/471855906699739136/sasp_logo_updated_2018.png")
+	
+    embed.add_field(name="".join(user_inpt3), value="[Click Here]({})".format(user_inpt4), inline=False)
+ 
+    await ctx.send(embed=embed)
+    await asyncio.sleep(2) 
+    await ctx.message.delete()
+
 @bot.command()
 async def clear(ctx, amount: int):
     await ctx.channel.purge(limit=amount)
@@ -57,128 +77,149 @@ async def clear(ctx, amount: int):
 async def status(ctx):
     web = requests.get('http://status.highspeed-gaming.com/')
     soup = BeautifulSoup(web.content, 'html.parser')
+    empserver = "No players currently online."
+    players1 = []
+    playerss2 = []
+    for element in soup.find_all("dd", id="accordion1"):
+      for stat in element.find_all("tr", attrs={"width":True}):
+        for players in stat.find_all("td")[1]:
+          players1.append(players)
+    if not players1:
+      players1.append(empserver)
+
+
+    for element2 in soup.find_all("dd", id="accordion2"):
+      for stat2 in element2.find_all("tr", attrs={"width":True}):
+        for players2 in stat2.find_all("td")[1]:
+          playerss2.append(players2)
+    if not playerss2:
+      playerss2.append(empserver)
+    
     servers = []
     for p in soup.find_all('a'):
       servers.append(p.text)
     embed = discord.Embed(title="__Highspeed-Gaming Status__", description=("Current Status of HSG Servers"), color=0x3D59AB)
     embed.set_author(name="State Police Info Bot", icon_url="https://cdn.discordapp.com/attachments/393324031505465344/471855906699739136/sasp_logo_updated_2018.png")
     embed.add_field(name="Server 1", value=servers[0], inline=False)
+    embed.add_field(name="S1 - Players", value=', '.join(players1), inline=False)
+
     embed.add_field(name="Server 2", value=servers[1], inline=False)
+    embed.add_field(name="S2 - Players", value=', '.join(playerss2), inline=False)
     await ctx.send(embed=embed)
 
 @bot.command()
 async def searchpc(ctx, *, arg):
-    temporary = """1.01 Inadmissible Defences
-1.02 Admissible Defences
-1.03 Persons Liable to Punishment
-2.01 Principals and Accessories
-2.02 Principles to a Crime
-2.03 Accessories to a Crime
-3.01 Bribery
-3.02 Felony Bribery
-3.03 Reception of a Bribe
-3.04 Felony Reception of a Bribe
-3.05 Obstruction of a Public Official
-3.06 Aggravated Obstruction of a Public Official
-4.01 Rescue
-4.02 Escape
-4.03 Aiding Escape
-4.04 Resisting Arrest
-4.05 Failure to Identify
-4.06 Obstruction of a Government Employee
-4.07 Misuse of a Government Hotline
-4.08 Tampering With Evidence
-4.09 Forgery of Public Record
-4.10 Perjury
-4.11 Falsification of Evidence
-4.12 Dissuading a Witness
-4.13 Evading
-4.14 Felony Reckless Evading
-4.15 Refusal to Sign
-4.16 Failure to Pay Fine
-4.17 Contempt of Court
-4.18 Provision of False Information to A State Employee
-4.19 False Complaint
-4.20 Human Trafficking
-4.21 Violation of Parole or Probation
-4.22 False Personation
-4.23 Peace Officer Refusing to Arrest
-4.24 False Report of Crime or Emergency
-4.25 Street Gang Membership
-5.01 Intimidation
-5.02 Assault
-5.03 Assault With a Deadly Weapon
-5.04 Mutual Combat
-5.05 Battery
-5.06 Aggravated Battery
-5.07 Attempted Murder
-5.08 Manslaughter
-5.09 Murder
-5.10 False Imprisonment
-5.11 Kidnapping
-5.12 Mayhem
-5.13 Hate Crime
-5.14 Hostage Taking
-6.01 Indecent Exposure
-6.02 Obscene Exhibition
-6.03 Bawdy or Disorderly House
-6.04 Prostitution
-6.05 Solicitation
-6.06 Pandering
-6.07 Sexual Assault
-6.08 Sexual Battery
-6.09 Rape
-6.10 Stalking
-6.11 Disorderly Conduct
-7.01 Possession of a Controlled Substance
-7.02 Possession of a Controlled Substance with Intent to Distribute
-7.03 Possession of Drug Paraphernalia
-7.04 Maintaining a Place for Purpose of Distribution
-7.05 Manufacture of a Controlled Substance
-7.06 Sale of a Controlled Substance
-7.07 Possession of an Open Container
-7.08 Public Intoxication
-7.09 Public Influence of a Controlled Substance
-7.10 Possession in Excess
-7.11 Criminal Threats
-7.12 Reckless Endangerment
-7.13 Trafficking of a Controlled Substance
-8.01 Obstruction of Assembly
-8.02 Riot
-8.03 Incitement to Riot
-8.04 Unlawful Assembly
-8.05 Breach of the Peace
-8.06 Obstruction of Rightful Passage
-9.01 Arson
-9.02 Burglary
-9.03 Possession of Burglary Tools
-9.04 Forgery
-9.05 Petty Theft
-9.06 Grand Theft
-9.07 Embezzlement
-9.08 Trespassing
-9.09 Robbery
-9.10 Armed Robbery
-9.11 Receiving Stolen Property
-9.12 Vandalism
-9.13 Criminal Damage
-9.14 Grand Larceny
-10.01 Hate Crime
-11.01 Extortion
-11.02 Money Laundering
-12.01 Unlawful Possession of a Firearm
-12.02 Unlawful Concealment of a Firearm
-12.03 Unlawful Carry of a Firearm
-12.04 Unlawful Display of a Firearm
-12.05 Unlawful Discharge of a Firearm
-12.06 Unlawful Possession of a Deadly Weapon
-12.07 Unlawful Display of a Deadly Weapon
-12.08 Unlawful Concealment of a Deadly Weapon
-12.09 Unlawful Possession of An Explosive Device
-12.10 Manufacture of an Explosive Device
-12.11 Unlawful Distribution of a Firearm
-12.12 Unlawful Possession of Firearms with Intent to Sell
-12.13 Unlawful Modification of a Firearm"""
+    temporary = """1.01 Inadmissible Defences	| 	N/A	| 	N/A
+1.02 Admissible Defences	| 	N/A	| 	N/A
+1.03 Persons Liable to Punishment	| 	N/A	| 	N/A	| 	
+2.01 Principals and Accessories	| 	N/A	| 	N/A
+2.02 Principles to a Crime	| 	N/A	| 	N/A
+2.03 Accessories to a Crime	| 	N/A	| 	N/A	| 	
+3.01 Bribery	| 	Misdemeanor	| 	Up to 3 Minutes
+3.02 Felony Bribery	| 	Felony	| 	From 5 to 10 Minutes
+3.03 Reception of a Bribe	| 	Misdemeanor	| 	Up to 3 Minutes
+3.04 Felony Reception of a Bribe	| 	Felony	| 	From 5 to 10 Minutes
+3.05 Obstruction of a Public Official	| 	Misdemeanor	| 	Up to 3 Minutes
+3.06 Aggravated Obstruction of a Public Official	| 	Felony	| 	Up to 10 Minutes	| 	
+4.01 Rescue	| 	Misdemeanor	| 	Up to 5 Minutes
+4.02 Escape	| 	Felony	| 	From 5 to 10 Minutes
+4.03 Aiding Escape	| 	Felony	| 	From 5 to 10 Minutes
+4.04 Resisting Arrest	| 	Misdemeanor	| 	Up to 3 Minutes
+4.05 Failure to Identify	| 	Misdemeanor	| 	10 Minutes
+4.06 Obstruction of a Government Employee	| 	Misdemeanor	| 	Up to 3 Minutes
+4.07 Misuse of a Government Hotline	| 	Infraction/Misdemeanor	| 	Up to 2 Minutes
+4.08 Tampering With Evidence	| 	Misdemeanor	| 	Up to 5 Minutes
+4.09 Forgery of Public Record	| 	Misdemeanor	| 	Up to 3 Minutes
+4.10 Perjury	| 	Felony	| 	From 8 to 15 Minutes
+4.11 Falsification of Evidence	| 	Felony	| 	From 5 to 10 Minutes
+4.12 Dissuading a Witness	| 	Misdemeanor	| 	Up to 5 Minutes
+4.13 Evading	| 	Misdemeanor	| 	Up to 5 Minutes
+4.14 Felony Reckless Evading	| 	Felony	| 	From 8 to 15 Minutes
+4.15 Refusal to Sign	| 	Misdemeanor	| 	From 5 to 15 Minutes
+4.16 Failure to Pay Fine	| 	Misdemeanor	| 	From 5 to 15 Minutes
+4.17 Contempt of Court	| 	Misdemeanor	| 	Up to 10 Minutes
+4.18 Provision of False Information to A State Employee	| 	Misdemeanor	| 	Up to 5 Minutes
+4.19 False Complaint	| 	Misdemeanor	| 	Up to 5 Minutes
+4.20 Human Trafficking	| 	Felony	| 	From 8 to 15 Minutes
+4.21 Violation of Parole or Probation	| 	Misdemeanor	| 	Up to 10 Minutes
+4.22 False Personation	| 	Felony	| 	Up to 5 Minutes
+4.23 Peace Officer Refusing to Arrest	| 	Felony	| 	Up to 5 Minutes
+4.24 False Report of Crime or Emergency	| 	Felony	| 	Up to 10 Minutes
+4.25 Street Gang Membership	| 	Felony	| 	From 8 to 15 Minutes	| 	
+5.01 Intimidation	| 	Misdemeanor	| 	Up to 5 Minutes
+5.02 Assault	| 	Misdemeanor	| 	Up to 5 Minutes
+5.03 Assault With a Deadly Weapon	| 	Misdemeanor/Felony	| 	Up to 5 Minutes/From 5 to 10 Minutes
+5.04 Mutual Combat	| 	Misdemeanor	| 	Up to 5 Minutes
+5.05 Battery	| 	Misdemeanor	| 	Up to 5 Minutes
+5.06 Aggravated Battery	| 	Misdemeanor	| 	Up to 10 Minutes
+5.07 Attempted Murder	| 	Felony	| 	From 8 to 15 Minutes
+5.08 Manslaughter	| 	Felony	| 	From 8 to 15 Minutes
+5.09 Murder	| 	Felony	| 	From 8 to 15 Minutes
+5.10 False Imprisonment	| 	Misdemeanor	| 	Up to 5 Minutes
+5.11 Kidnapping	| 	Felony	| 	From 8 to 18 Minutes
+5.12 Mayhem	| 	Felony	| 	From 8 to 15 Minutes
+5.13 Hate Crime	| 	Felony	| 	From 8 to 15 Minutes
+5.14 Hostage Taking	| 	Felony	| 	From 8 to 15 Minutes	| 		| 	
+6.01 Indecent Exposure	| 	Misdemeanor	| 	Up to 5 Minutes
+6.02 Obscene Exhibition	| 	Misdemeanor	| 	Up to 5 Minutes
+6.03 Bawdy or Disorderly House	| 	Misdemeanor	| 	Up to 5 Minutes
+6.04 Prostitution	| 	Misdemeanor	| 	Up to 5 Minutes
+6.05 Solicitation	| 	Misdemeanor	| 	Up to 5 Minutes
+6.06 Pandering	| 	Misdemeanor	| 	Up to 5 Minutes
+6.07 Sexual Assault	| 	Misdemeanor	| 	Up to 5 Minutes
+6.08 Sexual Battery	| 	Misdemeanor	| 	Up to 10 Minutes
+6.09 Rape	| 	Felony	| 	From 8 to 15 Minutes
+6.10 Stalking	| 	Misdemeanor	| 	Up to 5 Minutes
+6.11 Disorderly Conduct	| 	Misdemeanor	| 	Up to 5 Minutes
+7.01 Possession of a Controlled Substance	| 	Misdemeanor	| 	Up to 5 Minutes
+7.02 Possession of a Controlled Substance with Intent to Distribute	| 	Felony	| 	From 5 to 10 Minutes
+7.03 Possession of Drug Paraphernalia	| 	Misdemeanor	| 	Up to 3 Minutes
+7.04 Maintaining a Place for Purpose of Distribution	| 	Felony	| 	From 5 to 10 Minutes
+7.05 Manufacture of a Controlled Substance	| 	Felony	| 	From 5 to 15 Minutes
+7.06 Sale of a Controlled Substance	| 	Felony	| 	From 5 to 10 Minutes
+7.07 Possession of an Open Container	| 	Infraction	| 	N/A
+7.08 Public Intoxication	| 	Misdemeanor	| 	Up to 5 Minutes
+7.09 Public Influence of a Controlled Substance	| 	Misdemeanor	| 	Up to 5 Minutes
+7.10 Possession in Excess	| 	Misdemeanor	| 	Up to 5 Minutes
+7.11 Criminal Threats	| 	Misdemeanor	| 	Up to 5 Minutes
+7.12 Reckless Endangerment	| 	Misdemeanor	| 	Up to 5 Minutes
+7.13 Trafficking of a Controlled Substance	| 	Felony	| 	From 5 to 10 Minutes	| 	
+8.01 Obstruction of Assembly	| 	Misdemeanor	| 	Up to 5 Minutes
+8.02 Riot	| 	Misdemeanor	| 	Up to 10 Minutes
+8.03 Incitement to Riot	| 	Misdemeanor	| 	Up to 5 Minutes
+8.04 Unlawful Assembly	| 	Misdemeanor	| 	Up to 5 Minutes
+8.05 Breach of the Peace	| 	Misdemeanor	| 	Up to 5 Minutes
+8.06 Obstruction of Rightful Passage	| 	Misdemeanor	| 	Up to 5 Minutes
+9.01 Arson	| 	Felony	| 	From 8 to 15 Minutes
+9.02 Burglary	| 	Felony	| 	From 8 to 15 Minutes
+9.03 Possession of Burglary Tools	| 	Misdemeanor	| 	Up to 5 Minutes
+9.04 Forgery	| 	Misdemeanor/Felony	| 	Up to 5 Minutes/From 5 to 10 Minutes
+9.05 Petty Theft	| 	Misdemeanor	| 	Up to 10 Minutes
+9.06 Grand Theft	| 	Felony	| 	From 5 to 15 Minutes
+9.07 Embezzlement	| 	Misdemeanor/Felony	| 	Up to 10 Minutes/From 5 to 15 Minutes
+9.08 Trespassing	| 	Misdemeanor	| 	Up to 5 Minutes
+9.09 Robbery	| 	Felony	| 	From 5 to 15 Minutes
+9.10 Armed Robbery	| 	Felony	| 	From 8 to 15 Minutes
+9.11 Receiving Stolen Property	| 	Misdemeanor/Felony	| 	Up to 5 Minutes/From 5 to 10 Minutes
+9.12 Vandalism	| 	Misdemeanor	| 	Up to 5 Minutes
+9.13 Criminal Damage	| 	Misdemeanor	| 	Up to 5 Minutes
+9.14 Grand Larceny	| 	Felony	| 	From 5 to 15 Minutes	| 	
+10.01 Hate Crime	| 	Misdemeanor/Felony	| 	Up to 10 Minutes	| 		| 	
+11.01 Extortion	| 	Felony	| 	From 5 to 10 Minutes
+11.02 Money Laundering	| 	Felony	| 	From 5 to 10 Minutes	| 	
+12.01 Unlawful Possession of a Firearm	| 	Felony 	| 	From 5 to 10 Minutes
+12.02 Unlawful Concealment of a Firearm	| 	Misdemeanor/Felony	| 	Up to 5 Minutes/From 5 to 10 Minutes
+12.03 Unlawful Carry of a Firearm	| 	Misdemeanor/Felony	| 	Up to 5 Minutes/From 5 to 10 Minutes
+12.04 Unlawful Display of a Firearm	| 	Misdemeanor/Felony	| 	Up to 5 Minutes/From 5 to 10 Minutes
+12.05 Unlawful Discharge of a Firearm	| 	Misdemeanor/Felony	| 	Up to 5 Minutes/From 5 to 10 Minutes
+12.06 Unlawful Possession of a Deadly Weapon	| 	Misdemeanor/Felony	| 	Up to 5 Minutes/From 5 to 10 Minutes
+12.07 Unlawful Display of a Deadly Weapon	| 	Misdemeanor/Felony	| 	Up to 5 Minutes/From 5 to 10 Minutes
+12.08 Unlawful Concealment of a Deadly Weapon	| 	Misdemeanor/Felony	| 	Up to 5 Minutes/From 5 to 10 Minutes
+12.09 Unlawful Possession of An Explosive Device	| 	Felony	| 	From 5 to 15 Minutes
+12.10 Manufacture of an Explosive Device	| 	Felony	| 	From 8 to 15 Minutes
+12.11 Unlawful Distribution of a Firearm	| 	Misdemeanor/Felony	| 	Up to 10 Minutes/From 8 to 15 Minutes
+12.12 Unlawful Possession of FIrearms with Intent to Sell	| 	Felony	| 	From 5 to 15 Minutes.
+12.13 Unlawful Modification of a Firearm	| 	Misdemeanor/Felony	| 	Up to 5 Minutes/From 5 to 10 Minutes"""
     us_inpt = arg  
     err = "Nothing has been found based on your input"
     found_list = []
@@ -193,7 +234,7 @@ async def searchpc(ctx, *, arg):
 
     embed = discord.Embed(title="__San Andreas Penal Code Lookup__", description=("It looks like you searched for "+us_inpt.title()), color=0x3D59AB)
     embed.set_author(name="State Police Info Bot", icon_url="https://cdn.discordapp.com/attachments/393324031505465344/471855906699739136/sasp_logo_updated_2018.png")
-    embed.add_field(name="Here's what we found based on your search:", value=', '.join(found_list), inline=False)
+    embed.add_field(name="Here's what we found based on your search:", value='\n'.join(found_list), inline=False)
     embed23 = await ctx.send(embed=embed)
     await asyncio.sleep(10) 
     await ctx.message.delete()
@@ -201,51 +242,51 @@ async def searchpc(ctx, *, arg):
 	
 @bot.command()
 async def searchvc(ctx, *, arg):
-    temporary = """13.01 Reckless Driving
-13.01(b) Reckless Driving Causing Bodily Injury
-13.02 Eluding / Evading a Peace Officer
-13.03 Hit and Run
-13.03(b) Hit and Run With Injury
-13.04 Failure to Yield to a TCD
-13.05 Speeding
-13.06 Driving Wrong Way
-13.07 Illegal Window Tint
-13.08 Failure to Maintain Lane
-13.09 Impeding Traffic
-13.10 Open Alcohol Container
-13.10(b) Open Alcohol Container Under 21
-13.10(c) Driving in Possession of Marijuana
-13.11 Exhibition of Speed
-13.12 Illegal Vehicle Modifications
-13.13 Operating an Unroadworthy Vehicle
-13.14 Failure to display License Plate
-13.15 Failure to Yield to Emergency Vehicle(s)
-13.16 Unsafe Vehicle Load
-13.17 Driving on Sidewalk
-13.18 Interfering with Driver's Control of Vehicle
-13.19 Following too Closely
-13.20 Motorcycles: Lane Splitting
-13.21 Illegal Passing
-13.22 Unsafe Operation of Emergency Vehicle
-13.23 Driving Without Headlights at Night
-13.24 Failure to Dim Lights
-13.25 Driving Without a Valid License
-13.26 Failure to Show a Driver's License
-13.27 Illegal Parking
-13.28 Driving Under the Influence of Alcohol
-13.28(b) Driving Under the Influence of Drugs
-13.29 Broken Tail Light / Headlights
-13.30 Jaywalking
-13.31 Possession of Sirens & Emergency Lighting
-13.31(b) Possession of Sirens & Emergency Lighting With Intent to Impersonate
-13.32 Failure to Wear Motorcycle Helmet
-13.33 Hitchhiking
-13.34 Driving with a Suspended License
-13.35 Failure to Signal Lane Change
-13.36 Use of Hydraulics on Public Roads
-13.37 Intentionally Altering or Destroying a VIN
-13.38 Buying or Possessing a Vehicle with an Altered VIN
-13.39 Felony Speeding"""
+    temporary = """13.01 Reckless Driving  |   Misdemeanor/Infraction  |   Up to 3 Minutes / 150$ Fine / Both
+13.01(b) Reckless Driving Causing Bodily Injury |   Felony  |   p to 5 Minutes / 150$ Fine / Both
+13.02 Eluding / Evading a Peace Officer |   Misdemeanor/Infraction  |   Up to 3 Minutes / 150$ Fine / Both
+13.03 Hit and Run   |   Misdemeanor/Infraction  |   Up to 3 Minutes / 150$ Fine
+13.03(b) Hit and Run With Injury    |   Felony  |   Up to 6 Minutes / 150$ Fine / Both
+13.04 Failure to Yield to a TCD |   Infraction  |   150$ Fine
+13.05 Speeding  |   Infraction  |   150$ Fine
+13.06 Driving Wrong Way |   Infraction  |   150$ Fine
+13.07 Illegal Window Tint   |   Infraction  |   100$ Fine
+13.08 Failure to Maintain Lane  |   Infraction  |   150$ Fine
+13.09 Impeding Traffic  |   Infraction  |   150$ Fine
+13.10 Open Alcohol Container    |   Infraction  |   150$ Fine
+13.10(b) Open Alcohol Container Under 21    |   Misdemeanor/Infraction  |   Up to 5 Minutes / 150$ Fine / Both
+13.10(c) Driving in Possession of Marijuana |   Infraction  |   100$ Fine
+13.11 Exhibition of Speed   |   Infraction  |   150$ Fine
+13.12 Illegal Vehicle Modifications |   Infraction  |   150$ Fine
+13.13 Operating an Unroadworthy Vehicle |   Infraction  |   150$ Fine
+13.14 Failure to display License Plate  |   Infraction  |   150$ Fine
+13.15 Failure to Yield to Emergency Vehicle(s)  |   Misdemeanor/Infraction  |   Up to 5 Minutes / 150$ Fine / Both
+13.16 Unsafe Vehicle Load   |   Infraction  |   150$ Fine
+13.17 Driving on Sidewalk   |   Infraction  |   100$ Fine
+13.18 Interfering with Driver's Control of Vehicle  |   Misdemeanor/Infraction  |   Up to 2 Minutes / 150$ Fine
+13.19 Following too Closely |   Infraction  |   150$ Fine
+13.20 Motorcycles: Lane Splitting   |   Infraction  |   150$ Fine
+13.21 Illegal Passing   |   Infraction  |   150$ Fine
+13.22 Unsafe Operation of Emergency Vehicle    
+13.23 Driving Without Headlights at Night   |   Infraction  |   150$ Fine
+13.24 Failure to Dim Lights |   Infraction  150$ Fine
+13.25 Driving Without a Valid License   |   Misdemeanor/Infraction  |   Up to 5 Minutes / 150$ Fine / Both
+13.26 Failure to Show a Driver's License    |   Infraction  |   150$ Fine
+13.27 Illegal Parking      
+13.28 Driving Under the Influence of Alcohol    |   Misdemeanor |   Up to 6 Minutes / 150$ Fine
+13.28(b) Driving Under the Influence of Drugs   |   Misdemeanor |   Up to 6 Minutes / 150$ Fine
+13.29 Broken Tail Light / Headlights    |   Infraction  |   100$ Fine
+13.30 Jaywalking    |   Infraction  |   100$ Fine
+13.31 Possession of Sirens & Emergency Lighting |   Infraction  |   150$ Fine
+13.31(b) Possession of Sirens & Emergency Lighting With Intent to Impersonate   |   Misdemeanor |   Up to 6 Minutes / 150$ Fine
+13.32 Failure to Wear Motorcycle Helmet |   Infraction  |   150$ Fine
+13.33 Hitchhiking   |   Infraction  100$ Fine
+13.34 Driving with a Suspended License  |   Misdemeanor |   Up to 5 Minutes / 150 Fine
+13.35 Failure to Signal Lane Change |   Infraction  |   100$ Fine
+13.36 Use of Hydraulics on Public Roads |   Infraction  |   100$ Fine
+13.37 Intentionally Altering or Destroying a VIN    |   Misdemeanor/Infraction  |   Up to 5 Minutes / 150$ Fine / Both
+13.38 Buying or Possessing a Vehicle with an Altered VIN    |   Misdemeanor/Infraction  |   Up to 5 Minutes / 150$ Fine / Both
+13.39 Felony Speeding   |   Felony  |   Up to 5 Minutes"""
     us_inpt = arg  
     err = "Nothing has been found based on your input"
     found_list = []
@@ -259,7 +300,7 @@ async def searchvc(ctx, *, arg):
 
     embed = discord.Embed(title="__San Andreas Vehicle Code Lookup__", description=("It looks like you searched for "+us_inpt.title()), color=0x3D59AB)
     embed.set_author(name="State Police Info Bot", icon_url="https://cdn.discordapp.com/attachments/393324031505465344/471855906699739136/sasp_logo_updated_2018.png")
-    embed.add_field(name="Here's what we found based on your search:", value=', '.join(found_list), inline=False)
+    embed.add_field(name="Here's what we found based on your search:", value='\n'.join(found_list), inline=False)
     await ctx.send(embed=embed)
     await asyncio.sleep(10) 
     await ctx.message.delete()
